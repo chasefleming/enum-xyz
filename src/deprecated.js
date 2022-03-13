65fdac18b1e210426d280ccda29e264063e12f7b
Texas Toland <texastoland@gmail.com>
2022-03-13 23:04:42 +0800

    Add deprecated names
    Establishies and simplifiies memoization.
    `Symbols` is now memoized like `Integers`.

--- src/deprecated.js
+++ src/deprecated.js
@@ -1,1 +1,49 @@
-export let placeholder
+import { Counter, EnumOf, Integers, LowerCased, Strings } from './enum-xyz.js'
+
+/**
+ * @template T
+ * @param props {object}
+ * @param props.from {string}
+ * @param props.to {string}
+ * @param props.using {import('./enum-xyz.js').Enum<T>}
+ * @returns {import('./enum-xyz.js').Enum<T>}
+ */
+const deprecated = ({ from, to, using }) =>
+  EnumOf(
+    (name) => (
+      console.warn(`\`${from}\` is deprecated; use \`${to}\` instead`),
+      using[name]
+    )
+  )
+
+/** @deprecated */
+export const String = deprecated({
+  from: 'String',
+  to: 'Strings',
+  using: Strings,
+})
+
+/** @deprecated */
+export const StringLower = deprecated({
+  from: 'StringLower',
+  to: 'LowerCased',
+  using: LowerCased,
+})
+
+/** @deprecated */
+export const Numeric = deprecated({
+  from: 'Numeric',
+  to: 'Integers',
+  using: Integers,
+})
+
+/**
+ * @deprecated
+ * @param {number} startIndex
+ */
+export const NumericAt = (startIndex) =>
+  deprecated({
+    from: 'NumericAt',
+    to: 'Counter',
+    using: Counter(startIndex),
+  })
--- src/enum-xyz.js
+++ src/enum-xyz.js
@@ -1,32 +1,38 @@
+/** @template T @typedef {{ [name: string]: T }} Enum */
+
 /**
- * @template T
- * @param {(name: string) => T} mapper
- * @returns {{ [name: string]: T }}
+ * @template T, [TState=unknown]
+ * @param {(name: string, state: TState) => T} mapper should return the same value for the same name
+ * @param {TState} [state]
+ * @returns {Enum<T>}
  */
-export const EnumOf = (mapper) =>
+export const EnumOf = (mapper, state) =>
   new Proxy(
     {},
     {
       /** @param {string} name */
-      get: (_, name) => mapper(name),
+      get: (_, name) => mapper(name, /** @type {!TState} */ (state)),
     }
+  )
+
+/**
+ * @template T
+ * @param {(name: string) => T} mapper always returns the same value for the same name
+ */
+export const MemoEnumOf = (mapper) =>
+  EnumOf(
+    (name, map) =>
+      map.get(name) ??
+      /** @type {!T} */ (map.set(name, mapper(name)).get(name)),
+    /** @type {Map<string, T>} */ (new Map())
   )
 
 export const Strings = EnumOf((name) => name)
 
 export const LowerCased = EnumOf((name) => name.toLowerCase())
 
-export const Symbols = EnumOf(Symbol)
+export const Symbols = MemoEnumOf(Symbol)
 
-export const Counter = (startIndex = 0) => {
-  /** @type {Map<string, number>} */
-  const indexesByName = new Map()
-  return EnumOf(
-    (name) =>
-      /** @type {!number} nonnull */ (
-        indexesByName.set(name, indexesByName.size + startIndex).get(name)
-      )
-  )
-}
+export const Counter = (startIndex = 0) => MemoEnumOf((name) => startIndex++)
 
 export const Integers = Counter()
